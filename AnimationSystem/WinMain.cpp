97804cc258661f2d69282779424e8ce4aee7ac70
Eden Redman <46800990+Existentialist-Robot@users.noreply.github.com>
2022-03-09 12:06:27 -0700

    First commit

---
+++ AnimationSystem/WinMain.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#define WIN32_LEAN_AND_MEAN
+
+#define WIN32_EXTRA_LEAN
+
+
+#include <windows.h>
+#include <iostream>
+#include "Application.h"
+#include "glad.h"
+
